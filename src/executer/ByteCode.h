156ab91c2b87a970e7e1de942ca12d6fd33c1544
Moritz Goeckel <moritz.goeckel@gmail.com>
2025-06-01 01:29:22 +0200

    Document idea how to get rid of the dedicated locals and return_address struct

--- src/executer/ByteCode.h
+++ src/executer/ByteCode.h
@@ -15,6 +15,9 @@ using word_t = unsigned long;
 
 enum class Op { NOP, LOCALS, LOCALL, CALL, RET, PUSH, POP, WRITE_STACK, ADD, SUB, MUL, DIV, MOD, JUMP, JUMP_IF, ALLOC, WRITE_HEAP, READ_HEAP, PRINTS, TERM, LT, GT, EQ, LTE, GTE, NEQ };
 
+// TODO: ADD etc should be type specific, so IADD, FADD
+// TODO: Logic operator missing: AND, OR, NOT
+
 struct Instruction {
     Instruction(Op op, word_t arg1 = 0, word_t arg2 = 0, word_t arg3 = 0);
 
@@ -40,6 +43,32 @@ enum class ProgramState {
     Paused,
     Finished
 };
+
+// TODO: We don't need any of this, we can just use the stack
+// https://stackoverflow.com/questions/1395591/what-exactly-is-the-base-pointer-and-stack-pointer-to-what-do-they-point
+// https://www.cs.virginia.edu/~evans/cs216/guides/x86.html
+
+// We just need to introduce registers
+// esp: Current stack top
+// ebp: Stack base pointer, is set to esp in the beginning of a function
+// eax: Return value of the function
+
+// On function call:
+// - Push the parameters in reverse order
+// - Push return address (done by the CALL instruction)
+// - Push the current ebp
+// - Set ebp to esp
+// - Push locals
+// Within fucnction:
+// - Use ebp + n to access parameters
+// - Use ebp + n to access locals
+// On function return:
+// - Put result into eax register
+// - Pop locals from the stack
+// - Pop ebp from the stack, put into ebp register
+// - Pop return address from the stack, and jump (done by the RET instruction)
+// - Pop parameters from the stack (done by the caller, not the callee)
+// - Get result from eax register
 
 struct StackFrame {
     std::vector<word_t> locals;
