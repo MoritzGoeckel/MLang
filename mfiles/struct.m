2a1b4f3840129bb41592e927e9f49e6143657599
Moritz Goeckel <moritz.goeckel@gmail.com>
2025-06-03 01:41:08 +0200

    Nested struct example

--- mfiles/struct.m
+++ mfiles/struct.m
@@ -1,17 +1,26 @@
 # expect_result=3
 let i = 1;
 
-struct point {
+struct Point {
     let x: Int;
     let y: Int;
 }
 
 # TODO: Instatiate (heap allocation, garbage collection)
-let p: point;
+let p: Point;
 
 # TODO: Access fields
 # p.x = 1;
 # p.y = 2;
+
+struct Line {
+    let begin: Point;
+    let end: Point;
+}
+
+# TODO: Access nested fields
+let l: Line;
+# l.begin.x = 1;
 
 let j = 2;
 ret i + j;
